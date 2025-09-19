d5d00306ac5cedd9a28d9f7e73b7387b5d75f124
Maria Guillen <mguillen@catalyte.io>
2025-09-19 13:34:00 -0500

    added documentation

--- requirements.py
+++ requirements.py
@@ -50,70 +50,69 @@ submissions_list = [
 ]
 
 
-# # def filter_by_date(date, submissions_list):
-# #     """
-# #     Filters submissions by date when given a date and a list
-# #     Returns an empty list when no data is found
-# #     """
-# #     return [
-# #         student for student in submissions_list if student["submissionDate"] == date
-# #     ]
+def filter_by_date(date, submissions_list):
+    """
+    Filters submissions by date when given a date and a list
+    Returns an empty list when no data is found
+    """
+    return [
+        student for student in submissions_list if student["submissionDate"] == date
+    ]
 
 
-# # print(filter_by_date("09-01-2025", submissions_list))
+def filter_by_student_id(student_id, submissions_list):
+    """
+    Filters data by student ID when given a student ID and a list
+    Returns an empty list if no data is found
+    """
+    return [
+        student for student in submissions_list if student["studentID"] == student_id
+    ]
 
 
-# # def filter_by_student_id(student_id, submissions_list):
-# #     """
-# #     Filters data by student ID when given a student ID and a list
-# #     Returns an empty list if no data is found
-# #     """
-# #     return [
-# #         student for student in submissions_list if student["studentID"] == student_id
-# #     ]
+def find_unsubmitted(date, student_name, submissions_list):
+    """
+    Given a date and a list of names,
+    this function returns a list of those students who have not completed a quiz
+    on the given date
+    """
+    students_who_submitted = [
+        student["studentName"]
+        for student in submissions_list
+        if student["submissionDate"] == date
+    ]
+    not_submitted = [
+        student for student in student_name if student not in students_who_submitted
+    ]
+    return not_submitted
 
 
-# # print(filter_by_student_id(123, submissions_list))
-
-
-# def find_unsubmitted(date, student_name, submissions_list):
-#     students_who_submitted = [
-#         student["studentName"]
-#         for student in submissions_list
-#         if student["submissionDate"] == date
-#     ]
-#     not_submitted = [
-#         student for student in student_name if student not in students_who_submitted
-#     ]
-#     return not_submitted
-
-
-# # print(find_unsubmitted("09-12-2025", ["John", "Richard"], submissions_list))
-
-
-# def get_average_score(submissions_list):
-#     sum_of_scores = sum(student["quizScore"] for student in submissions_list)
-#     return round((sum_of_scores / len(submissions_list)), 2)
-
-
-# print(get_average_score(submissions_list))
+def get_average_score(submissions_list):
+    """
+    Returns the average score for all quizes
+    """
+    sum_of_scores = sum(student["quizScore"] for student in submissions_list)
+    return round((sum_of_scores / len(submissions_list)), 2)
 
 
 def get_average_score_by_module(submissions_list):
+    """
+    Returns the name of the module followed by the average score for that particular module
+    """
     scores_by_module = {}
     total_quizes_by_module = {}
     for submission in submissions_list:
-        module = submission["quizModule"]
-        score = submission["quizScore"]
-        scores_by_module[module] += score
-        total_quizes_by_module[module] += 1
+        quiz_module = submission["quizModule"]
+        quiz_score = submission["quizScore"]
+        if quiz_module not in scores_by_module:
+            scores_by_module[quiz_module] = 0
+            total_quizes_by_module[quiz_module] = 0
+        scores_by_module[quiz_module] += quiz_score
+        total_quizes_by_module[quiz_module] += 1
 
     module_average = {}
-    for module in scores_by_module:
-        module_average[module] = round(
-            scores_by_module[module] / total_quizes_by_module[module], 2
+    for quiz_module in scores_by_module:
+        module_average[quiz_module] = round(
+            scores_by_module[quiz_module] / total_quizes_by_module[quiz_module], 2
         )
     return module_average
-
-
-print(get_average_score_by_module(submissions_list))
