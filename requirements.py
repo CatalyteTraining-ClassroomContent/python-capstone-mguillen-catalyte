9c2dada14f5d8daf0543336bd9c1150f78e42fa2
Maria Guillen <mguillen@catalyte.io>
2025-09-19 10:26:39 -0500

    completed filter by date function

--- requirements.py
+++ requirements.py
@@ -1,12 +1,70 @@
-def filter_by_date():
+submissions_list = [
+    {
+        "quizName": "Slope Quiz",
+        "quizModule": "Math",
+        "quizScore": 95,
+        "studentID": 147,
+        "studentName": "Maria",
+        "submissionDate": "09-05-2025",
+    },
+    {
+        "quizName": "Linear Equations Quiz",
+        "quizModule": "Math",
+        "quizScore": 98,
+        "studentID": 185,
+        "studentName": "Laura",
+        "submissionDate": "09-04-2025",
+    },
+    {
+        "quizName": "World War I Quiz",
+        "quizModule": "History",
+        "quizScore": 89,
+        "studentID": 196,
+        "studentName": "David",
+        "submissionDate": "09-10-2025",
+    },
+    {
+        "quizName": "Biology Quiz",
+        "quizModule": "Science",
+        "quizScore": 92,
+        "studentID": 136,
+        "studentName": "Richard",
+        "submissionDate": "09-12-2025",
+    },
+    {
+        "quizName": "Chemistry Quiz",
+        "quizModule": "Science",
+        "quizScore": 85,
+        "studentID": 125,
+        "studentName": "John",
+        "submissionDate": "09-12-2025",
+    },
+    {
+        "quizName": "American History Quiz",
+        "quizModule": "History",
+        "quizScore": 95,
+        "studentID": 147,
+        "studentName": "Maria",
+        "submissionDate": "09-10-2025",
+    },
+]
 
 
-def filter_by_student_id(): 
+def filter_by_date(date, submission):
+    """
+    Filters submissions by date
+    """
+    return [sub for sub in submissions_list if sub["submissionDate"] == date]
 
 
-def find_unsubmitted(): 
+# print(filter_by_date("09-12-2025", submissions_list))
 
-def get_average_score():
 
-def get_average_score_by_module(): 
+# def filter_by_student_id():
 
+
+# def find_unsubmitted():
+
+# def get_average_score():
+
+# def get_average_score_by_module():
